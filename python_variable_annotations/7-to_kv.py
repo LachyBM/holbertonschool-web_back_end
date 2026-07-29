523140fefb2a1be1ddc1346c3b770947676e0c92
LachyBM <13072@holbertonstudents.com>
2026-07-29 20:16:04 +1000

    fixed kv

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """module to takes string"""
 
-from typing import Union,List
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int,float]) -> Tuple(str,float)
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """function that sqaures"""
     return (k, v ** 2)
