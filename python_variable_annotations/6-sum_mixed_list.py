a7b89043a48883180196b5d9a5aead11712fa360
LachyBM <13072@holbertonstudents.com>
2026-07-29 20:12:38 +1000

    fixed anno

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -4,6 +4,6 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """funtion to get sum"""
     return sum(mxd_lst)
