3cb89122b25266a8842489c5202554a77aacaa5c
LachyBM <13072@holbertonstudents.com>
2026-08-02 12:11:56 +1000

    added dir and task0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""module for delay"""
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """async generator that yields a random number between 0 and 10"""
+    for n in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
