3bef7f1d6c7f4bfc169349e5ca5f6fd407576420
LachyBM <13072@holbertonstudents.com>
2026-08-02 12:37:43 +1000

    betty is misery

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-""" module for async"""
+"""module for async"""
 
 from typing import List
 
-async_gen = __import__('0-async_generator').async_generator
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
     """returns list of random 10"""
-    return [i async for i in async_gen()]
+    return [i async for i in async_generator()]
