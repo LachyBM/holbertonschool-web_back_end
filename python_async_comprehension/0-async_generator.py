f5899df1c6a68a49444991df57b9f06601d3a1e9
LachyBM <13072@holbertonstudents.com>
2026-08-02 12:34:39 +1000

    im actually lost

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""module for delay"""
+"""module for async generator"""
 
 import asyncio
 import random
@@ -7,7 +7,7 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """async generator that yields a random number be, nontween 0 and 10"""
-    for _ in range(10):
+    """10 random numbers between 0 and 10 async"""
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
