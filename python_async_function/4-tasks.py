89fd68c666ec00e62c268cd7a02adf825a4b1733
LachyBM <13072@holbertonstudents.com>
2026-08-02 11:56:48 +1000

    added tasks

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""module for multi taks"""
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """using wait random to create a list of tasks
+    """
+    delays = []
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+
+    return delays
