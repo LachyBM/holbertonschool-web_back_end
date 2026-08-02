5625f49ec014148b5a44e81ec21e2c73a1a362c5
LachyBM <13072@holbertonstudents.com>
2026-08-02 12:41:20 +1000

    task 2

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#1/usr/bin/env python3  
+"""module for runtime measure"""
+
+import asyncio
+import time 
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """executes async commp 4 times and measure total runtime"""
+    start = time.time()
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+    end = time.time()
+    return end - start
