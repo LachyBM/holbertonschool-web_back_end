58541c4dd609ed13ac1a291df6aefa247a940e61
LachyBM <13072@holbertonstudents.com>
2026-08-11 20:06:25 +1000

    fixed rreturn

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -6,4 +6,5 @@ from typing import Tuple, List
 
 def index_range(page: int, page_size: int) -> tuple[int, int]:
     """function that returns indexs"""
-    return ((page - 1)*page_size, page * page_size)
+
+    return ((page - 1) * page_size), page * page_size
