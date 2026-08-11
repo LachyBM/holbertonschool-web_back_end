1d64ff9fc035b4a33de6f05ece39517d9c86af63
LachyBM <13072@holbertonstudents.com>
2026-08-11 22:28:45 +1000

    added 1

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,44 @@
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+
+        assert isinstance(page, int)
+        assert isinstance(page_size, int)
+        assert page > 0
+        assert page_size > 0
+
+        start_index, end_index = index_range(page, page_size)
+        data = self.dataset()
+
+        if start_index >= len(data):
+            return []
+
+        return data[start_index:end_index]
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """function that returns indexs"""
+
+    return ((page - 1) * page_size), page * page_size
