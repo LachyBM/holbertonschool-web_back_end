8d7f9d60a92a2f7da101625b5af790d5ee5442a7
LachyBM <13072@holbertonstudents.com>
2026-08-11 20:04:59 +1000

    fixed?

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,6 +2,8 @@
 """ module to make a pagination helper function"""
 
 from typing import Tuple, List
-"""function that returns indexs"""
-def index_range(page: int, page_size: int) ->Tuple[int,int]:
-    return ((page - 1)*page_size,page * page_size)
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """function that returns indexs"""
+    return ((page - 1)*page_size, page * page_size)
