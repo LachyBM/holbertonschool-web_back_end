0c5291776c87702639468e60852b31067d493255
LachyBM <13072@holbertonstudents.com>
2026-08-16 15:47:25 +1300

    added 120

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""changes topics"""
+
+
+def updates_topics(mongo_collection, name, topics):
+    """update the topics in school"""
+    mongo_collection.update_many(
+            {"name":name},
+            {"$set":{"topics":topics})
+            )
+
