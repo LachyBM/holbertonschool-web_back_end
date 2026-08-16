421f1523a4c274990b31115f2f56bc1672f8cf58
LachyBM <13072@holbertonstudents.com>
2026-08-16 23:39:43 +1000

    added task 12

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" 11-main """
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+
+    count_logs = nginx_collection.count_documents({})
+    print(f"{count_logs} logs")
+
+    print("Methods:")
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for methods in method:
+        count_methods = nginx_collection.count_documents({"method": methods})
+        print(f"\tmethod {methods}: {count_methods}")
+
+    sc = nginx_collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{sc} status check")
