b1d548cc0e25d6e0c04496d71248caa7a69aa5c7
LachyBM <13072@holbertonstudents.com>
2026-08-29 16:18:26 +1000

    added 9

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+    return new Map([
+        ['Tomatoes', 10],
+        ['Pasta', 1],
+        ['Rice', 1],
+        ['Apples', 10],
+        ['Banana', 5],
+    ])
+}
