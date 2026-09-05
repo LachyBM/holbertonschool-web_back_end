d51e5a507051ef5e16db52940e5b419bd4d69b2b
LachyBM <13072@holbertonstudents.com>
2026-09-06 02:24:41 +1000

    fixe?

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
-console.log(`Welcome to Holberton School, what is your name?`);
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
