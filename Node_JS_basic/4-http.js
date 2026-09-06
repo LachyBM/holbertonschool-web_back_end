a2a9080df1060065dd962bad58cb5ece3b60c741
LachyBM <13072@holbertonstudents.com>
2026-09-06 12:34:11 +1000

    fixed4?

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -3,12 +3,14 @@ const http = require('http');
 const HOSTNAME = 'localhost';
 const PORT = 1245;
 
-const server = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-server.listen(PORT, HOSTNAME, () => {
+app.listen(PORT, HOSTNAME, () => {
   console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
 });
+
+module.exports = app;
